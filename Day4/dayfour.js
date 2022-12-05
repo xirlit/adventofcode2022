3374c4827010fa10c71a52586ffc70f7f9d4f034
xirlit <karel.llukas@gmail.com>
2022-12-05 22:37:17 +0100

    day four

---
+++ Day4/dayfour.js
@@ -0,0 +1,34 @@
+// partOne - 424
+// partTwo - 2716
+
+const fs = require('fs');
+const path = require('node:path');
+
+const getRange = (start, end) => {
+    return Array.from({ length: end - start }, (_, i) => start + i);
+}
+
+const getSections = (pair) => {
+    let [firstElf, secondElf] = pair.split(',');
+
+    let firstElfSections = getRange(parseInt(firstElf.split('-')[0]), parseInt(firstElf.split('-')[1]) + 1);
+    let secondElfSections = getRange(parseInt(secondElf.split('-')[0]), parseInt(secondElf.split('-')[1]) + 1);
+
+    return { firstElfSections, secondElfSections }
+}
+
+const dayFour = (partOne) => {
+    const data = fs.readFileSync(path.resolve(__dirname, 'data.dat'), 'utf-8');
+
+    return data.split("\r\n").filter(pair => {
+        let { firstElfSections, secondElfSections } = getSections(pair);
+
+        return (partOne) ?
+            firstElfSections.every(element => secondElfSections.includes(element)) ||
+            secondElfSections.every(element => firstElfSections.includes(element)) :
+            firstElfSections.some(element => secondElfSections.includes(element)) ||
+            secondElfSections.some(element => firstElfSections.includes(element));
+    }).length;
+}
+
+exports.dayFour = dayFour;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { dayOne } = require('./Day1/dayone.js');
 const { dayTwo } = require('./Day2/daytwo.js');
 const { dayThree } = require('./Day3/daythree.js');
+const { dayFour } = require('./Day4/dayfour.js');
 
 // Day 1
 console.log(`dayone (part1): ${dayOne(true)}`);
@@ -13,3 +14,7 @@ console.log(`daytwo (part2): ${dayTwo(false)}`);
 // Day 3
 console.log(`daythree (part1): ${dayThree(true)}`);
 console.log(`daythree (part2): ${dayThree(false)}`);
+
+// Day 4
+console.log(`dayfour (part1): ${dayFour(true)}`);
+console.log(`dayfour (part2): ${dayFour(false)}`);
